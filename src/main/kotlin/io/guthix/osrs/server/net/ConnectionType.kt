2892a9ab435c632c667984650d068d67f8d05b6d
Bart van Helvert <B.A.J.v.Helvert@student.tue.nl>
2019-01-26 23:47:37 +0100

    Add license header

--- src/main/kotlin/io/guthix/osrs/server/net/ConnectionType.kt
+++ src/main/kotlin/io/guthix/osrs/server/net/ConnectionType.kt
@@ -1,8 +1,26 @@
+/*
+OSRS-Server
+Copyright (C) 2019 Guthix
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package io.guthix.osrs.server.net
 
 import io.guthix.osrs.server.revision
 import io.netty.buffer.ByteBuf
 import io.netty.channel.ChannelHandlerContext
+import io.netty.channel.ChannelPipeline
 import io.netty.channel.SimpleChannelInboundHandler
 import io.netty.handler.codec.ByteToMessageDecoder
 import java.io.IOException
@@ -35,18 +53,25 @@ class ConnectionTypeHandler : SimpleChannelInboundHandler<IncPacket>() {
     override fun channelRead0(ctx: ChannelHandlerContext, msg: IncPacket) {
         when(msg) {
             is RevisionHandshakePacket -> {
-                ctx.pipeline().addAfter(
-                    ConnectionTypeDecoder::class.qualifiedName,
-                    StatusResponseEncoder::class.qualifiedName,
-                    StatusResponseEncoder()
-                )
+                ctx.pipeline().addStatusResponseEncoder()
                 if(msg.revision != revision) {
                     ctx.channel().writeAndFlush(StatusResponse.OUT_OF_DATE)
                     ctx.close()
                 } else {
                     ctx.channel().writeAndFlush(StatusResponse.SUCCESSFUL)
+                    ctx.pipeline().swapToJS5()
                 }
             }
         }
+    }
+
+    private fun ChannelPipeline.addStatusResponseEncoder() = addAfter(
+        ConnectionTypeDecoder::class.qualifiedName,
+        StatusResponseEncoder::class.qualifiedName,
+        StatusResponseEncoder()
+    )
+
+    private fun ChannelPipeline.swapToJS5() {
+        TODO()
     }
 }
